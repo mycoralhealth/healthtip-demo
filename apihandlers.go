35205de5d965bb7c684b2b597c48eef5ae7e54ba
nosequeldeebee <nosequeldeebee@users.noreply.github.com>
2017-12-30 14:37:13 -0800

    more scaffolding for api handlers

---
+++ apihandlers.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"database/sql"
+	"net/http"
+)
+
+func handleWriteUser(w http.ResponseWriter, r *http.Request, dbCon *sql.DB) {
+}
+func handleUpdateUser(w http.ResponseWriter, r *http.Request, dbCon *sql.DB) {
+}
+func handleLogin(w http.ResponseWriter, r *http.Request, dbCon *sql.DB) {
+}
+func handleLogout(w http.ResponseWriter, r *http.Request, dbCon *sql.DB) {
+}
+func handleRecords(w http.ResponseWriter, r *http.Request, dbCon *sql.DB) {
+}
+func handleSingleRecord(w http.ResponseWriter, r *http.Request, dbCon *sql.DB) {
+}
