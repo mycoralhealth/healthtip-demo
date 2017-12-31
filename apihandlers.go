de5be76a2725262c5cafb1dc6d1d55c84baeabc1
nosequeldeebee <nosequeldeebee@users.noreply.github.com>
2017-12-30 16:33:04 -0800

    done users api

--- apihandlers.go
+++ apihandlers.go
@@ -27,7 +27,24 @@ func handleWriteUser(w http.ResponseWriter, r *http.Request, dbCon *sql.DB) {
 }
 
 func handleUpdateUser(w http.ResponseWriter, r *http.Request, dbCon *sql.DB) {
+	var user User
+
+	decoder := json.NewDecoder(r.Body)
+	if err := decoder.Decode(&user); err != nil {
+		handleError(w, r, http.StatusBadRequest, err.Error())
+		return
+	}
+	defer r.Body.Close()
+
+	if err := updateUser(dbCon, user); err != nil {
+		handleError(w, r, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	respondWithJSON(w, r, http.StatusCreated, user)
+
 }
+
 func handleLogin(w http.ResponseWriter, r *http.Request, dbCon *sql.DB) {
 }
 func handleLogout(w http.ResponseWriter, r *http.Request, dbCon *sql.DB) {
--- db.go
+++ db.go
@@ -1,13 +1,21 @@
 package main
 
 import (
+	"crypto/sha256"
 	"database/sql"
+	"encoding/hex"
 	"fmt"
 )
 
 func writeUser(dbCon *sql.DB, u User) (int64, error) {
 
-	result, err := dbCon.Exec(`INSERT INTO users (email, first_name, last_name, password) VALUES ($1, $2, $3, $4);`, u.Email, u.First_name, u.Last_name, u.Password)
+	// check if user exists and return error if it does
+	// if checkUserExists function does NOT throw an error it means the user already exists
+	if err := checkUserExists(dbCon, u); err == nil {
+		return 0, fmt.Errorf("user %v already exists", u.Email)
+	}
+
+	result, err := dbCon.Exec(`INSERT INTO users (email, first_name, last_name, password) VALUES ($1, $2, $3, $4);`, u.Email, u.First_name, u.Last_name, hashPassword(u.Password))
 	if err != nil {
 		return 0, fmt.Errorf("couldn't insert %v into users table: %v", u.Email, err)
 	}
@@ -18,4 +26,30 @@ func writeUser(dbCon *sql.DB, u User) (int64, error) {
 	}
 
 	return ID, nil
+}
+
+func updateUser(dbCon *sql.DB, u User) error {
+
+	_, err := dbCon.Exec(`UPDATE users SET first_name = $1, last_name = $2, password = $3;`, u.First_name, u.Last_name, hashPassword(u.Password))
+	if err != nil {
+		return fmt.Errorf("couldn't update %v in users table: %v", u.Email, err)
+	}
+
+	return nil
+}
+
+func checkUserExists(dbCon *sql.DB, u User) error {
+	var user User
+	if err := dbCon.QueryRow(`SELECT ROWID FROM users WHERE email = $1;`, u.Email).Scan(&user); err == sql.ErrNoRows {
+		return fmt.Errorf("user not found: %v", u.Email)
+	}
+	return nil
+
+}
+
+func hashPassword(password string) string {
+	hash := sha256.New()
+	hash.Write([]byte(password))
+	md := hash.Sum(nil)
+	return hex.EncodeToString(md)
 }
