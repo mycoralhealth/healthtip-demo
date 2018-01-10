8305e5cdcace42c4c1895d4b3c01c9e8b0a8eef3
Nikola Grcevski <ngrcevski@venasolutions.com>
2018-01-10 13:17:36 -0500

    Remove sensitive file.

--- sendtestemail.sh
+++
@@ -1,6 +0,0 @@
-curl -s --user 'api:key-d109e953a537e26356d3bf45a31ae065' \
-    https://api.mailgun.net/v3/mail.mycoralhealth.com/messages \
-    -F from='Health Tips <mailgun@mail.mycoralhealth.com>' \
-    -F to=andy@mycoralhealth.com \
-    -F subject='Hello here is a health tip' \
-    -F text='Testing some Mailgun awesomeness!'
