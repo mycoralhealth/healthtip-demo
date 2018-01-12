84e04e5dc45367810bb48be91e081f5f07ab7e37
Nikola Grcevski <ngrcevski@venasolutions.com>
2018-01-11 20:38:26 -0500

    Fix indenting.

--- mailgun.go
+++ mailgun.go
@@ -2,8 +2,8 @@ package main
 
 import (  
 	"gopkg.in/mailgun/mailgun-go.v1"
-    "os"
-    "fmt"
+	"os"
+	"fmt"
 	"strconv"
 	"log"
 )
@@ -16,29 +16,29 @@ func emailHealthTipRequest(user User, record Record) error {
 
 	mailgun.Debug = true
 
-    mg := mailgun.NewMailgun(os.Getenv("MG_DOMAIN"), os.Getenv("MG_API_KEY"), "")
+	mg := mailgun.NewMailgun(os.Getenv("MG_DOMAIN"), os.Getenv("MG_API_KEY"), "")
 	message := mg.NewMessage(
-	    "Health Tips <no-reply@mail.mycoralhealth.com>",
-	    "New Health Tip Request",
-	    "Hello, we received a new Health Tip request. Details as follows.\n\nUser: " + user.First_name + " " + user.Last_name +
-	    "\nEmail: " + user.Email + "\n" +
-	    "Test result details: \n" + 
-	    "  Age: " + strconv.Itoa(record.Age) + "\n" + 
-	    "  Height: " + strconv.Itoa(record.Height) + "\n" + 
-	    "  Weight: " + strconv.Itoa(record.Weight) + "\n" + 
-	    "  Cholesterol: " + strconv.Itoa(record.Weight) + "\n" + 
-	    "  Blood pressure: " + strconv.Itoa(record.Blood_pressure) + "\n\n" + 
-	   	"Please respond in 24 hours as per our demo website SLA.\nThanks!",
-	    os.Getenv("MAIL_TO"))
+		"Health Tips <no-reply@mail.mycoralhealth.com>",
+		"New Health Tip Request",
+		"Hello, we received a new Health Tip request. Details as follows.\n\nUser: " + user.First_name + " " + user.Last_name +
+		"\nEmail: " + user.Email + "\n" +
+		"Test result details: \n" + 
+		"  Age: " + strconv.Itoa(record.Age) + "\n" + 
+		"  Height: " + strconv.Itoa(record.Height) + "\n" + 
+		"  Weight: " + strconv.Itoa(record.Weight) + "\n" + 
+		"  Cholesterol: " + strconv.Itoa(record.Weight) + "\n" + 
+		"  Blood pressure: " + strconv.Itoa(record.Blood_pressure) + "\n\n" + 
+		"Please respond in 24 hours as per our demo website SLA.\nThanks!",
+		os.Getenv("MAIL_TO"))
 	
 	resp, id, err := mg.Send(message)
 	
 	if err != nil {
 		log.Println(err)
-	    return err
+		return err
 	}
 
 	fmt.Printf("ID: %s Resp: %s\n", id, resp)
 
-    return nil
+	return nil
 }
